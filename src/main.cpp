209d8ca9a558764b478dc096b9422b53e4d41f43
Luiz <luiz@aluno.unb.br>
2019-05-13 14:09:02 -0300

    EQU and MACRO implemented

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,11 @@ typedef enum {
 	SUM
 } operation;
 
+typedef enum {
+	NORMAL, 
+	MACRO
+} ppState;
+
 class Expression {
 	public:
 	string op1;
@@ -20,27 +25,63 @@ class Expression {
 	operation op;
 };
 
+class Symbol {
+	public:
+	int value;
+	string str;
+};
+
+
 class Statement {
 	public:
 	string line;
 	string label;
 	string op;
-	Expression arg1;
-	Expression arg2;
+	Expression arg[3];
 	string comment;
 	int lineNumber;
-};
-
-class Symbol {
-	public:
-	int value;
+	int countArgs(){
+		if(this->arg[0].op1.empty()) return 0;
+		else if(this->arg[1].op1.empty()) return 1;
+		else if(this->arg[2].op1.empty()) return 2;
+		else return 3;
+	}
+	Statement subst(map<string, Symbol> toSubst){
+		Statement ret = *this;
+		for(auto it = toSubst.begin() ; it != toSubst.end() ; it++){
+			for(int i=0 ; i<3 ; i++){
+				if(ret.arg[i].op1 == it->first)
+					ret.arg[i].op1 = it->second.str;
+				if(ret.arg[i].op2 == it->first)
+					ret.arg[i].op2 = it->second.str;
+			}
+		}
+		return ret;
+	}
+	void print(){
+		cout << "Line: \"" << (*this).line << "\"\n";
+		cout << "\tLine Number: " << (*this).lineNumber << "\n";
+		cout << "\tLabel: \"" << (*this).label << "\"\n";
+		cout << "\tOperation: \"" << (*this).op << "\"\n";
+		cout << "\targ[0]_op1: \"" << (*this).arg[0].op1 << "\"\n";
+		cout << "\targ[0]_op: " << ((*this).arg[0].op ? "SUM" : "NONE") << "\n";
+		cout << "\targ[0]_op2: \"" << (*this).arg[0].op2 << "\"\n";
+		cout << "\targ[1]_op1: \"" << (*this).arg[1].op1 << "\"\n";
+		cout << "\targ[1]_op: " << ((*this).arg[1].op ? "SUM" : "NONE") << "\n";
+		cout << "\targ[1]_op2: \"" << (*this).arg[1].op2 << "\"\n";
+		cout << "\targ[2]_op1: \"" << (*this).arg[2].op1 << "\"\n";
+		cout << "\targ[2]_op: " << ((*this).arg[2].op ? "SUM" : "NONE") << "\n";
+		cout << "\targ[2]_op2: \"" << (*this).arg[2].op2 << "\"\n";
+		cout << "\tComment: \"" << (*this).comment << "\"\n";
+	}
 };
 
 class MDT {
 	public:
 	int nargs;
+	string arg[3];
 	list<Statement> lstmt;
-}
+};
 
 string strToLower(string data){
 	std::transform(data.begin(), data.end(), data.begin(), ::tolower);
@@ -48,35 +89,59 @@ string strToLower(string data){
 }
 
 list<Statement> getStatementList(ifstream *file){
-	regex lineRegex("(?:\\s|\\t)*(?:(\\w*):)?(?:\\s|\\t)*(?:(\\w*):)?(?:\\s|\\t)*(?:(?:\\w*):)*(?:(?:\\s|\\t)*([^\\s|\\t|;]*)(?:(?:\\s|\\t)*(?:(?:\\+)?((?:-)?[^\\s|\\t|\\+|,|;]*))(?:\\s|\\t)*(?:(\\+)(?:\\s|\\t)*([^\\s|\\t|,|;]*))?)?(?:,(?:\\s|\\t)*([^\\s|\\t|\\+|;]*)(?:(\\+)([^\\s|\\t|;]*))?)?[^;]*)(?:;(.*))?");
+	regex lineRegex("(?:\\s|\\t)*(?:(\\w*):)?(?:\\s|\\t)*(?:(\\w*):)?(?:\\s|\\t)*(?:(?:\\w*):)*(?:(?:\\s|\\t)*([^\\s|\\t|;]*)(?:(?:\\s|\\t)*(?:(?:\\+)?((?:-)?[^\\s|\\t|\\+|,|;]*))(?:\\s|\\t)*(?:(\\+)(?:\\s|\\t)*([^\\s|\\t|,|;]*))?)?(?:,(?:\\s|\\t)*([^\\s|\\t|\\+|,|;]*)(?:(\\+)([^\\s|\\t|,|;]*))?)?(?:,(?:\\s|\\t)*([^\\s|\\t|\\+|,|;]*)(?:(\\+)([^\\s|\\t|;]*))?)?[^;]*)(?:;(.*))?"); //!TODO: Macro's args should begin with &
+	
+	regex replaceTabs("\\t");
+	regex removeSpaces("(\\s){2,}");
+	regex removeInitialSpace("^(\\s)");
+	regex removeFinalSpace("(\\s)$");
+	regex removeSpaceNotBetweenWordsRight("([^\\w])(\\s)");
+	regex removeSpaceNotBetweenWordsLeft("(\\s)([^\\w])");
+	
+	string matchArgument = "(?:(?:\\+)?((?:-)?[^\\s|\\+|,|;]*))(?:(\\+)([^\\s|,|;]*))?";
+	regex matchStatement("(?:(\\w*):)?([^\\s||;])\\s" + matchArgument + "(?:," + matchArgument + ")?(?:," + matchArgument + ")?[^;]*(?:;(.*))?");
+	
 	list<Statement> statements;
 	smatch lineMatch;
 	string line;
+	string semespacos;
 
 	for(int lineNumber=1 ; getline(*file, line) ; lineNumber++){
+	
+		semespacos = regex_replace(regex_replace(regex_replace(regex_replace(regex_replace(regex_replace(line, replaceTabs, " "), removeSpaces, " "), removeInitialSpace, ""), removeFinalSpace, ""), removeSpaceNotBetweenWordsRight, "$01"), removeSpaceNotBetweenWordsLeft, "$02");
+		
+		//cout << "Sem espaços: \"" <<  semespacos << "\"\n";
+	
 		if(regex_search(line, lineMatch, lineRegex)){
 			Statement statement;
-			Expression arg1, arg2;
+			Expression arg[3];
 			statement.line = line;
 			statement.label = strToLower(lineMatch.str(1));
 			if(!lineMatch.str(2).empty()) 
 				cout << "Syntax Error: Multiple labels in the same statement in line " << lineNumber << "\n";
 			statement.op = strToLower(lineMatch.str(3));
-			arg1.op1 = strToLower(lineMatch.str(4));
+			arg[0].op1 = strToLower(lineMatch.str(4));
 			if(!lineMatch.str(5).empty() && !lineMatch.str(6).empty()){
-				arg1.op = SUM;
-				arg1.op2 = strToLower(lineMatch.str(6));
+				arg[0].op = SUM;
+				arg[0].op2 = strToLower(lineMatch.str(6));
 			}
-			else arg1.op = NONE;
-			arg2.op1 = strToLower(lineMatch.str(7));
+			else arg[0].op = NONE;
+			arg[1].op1 = strToLower(lineMatch.str(7));
 			if(!lineMatch.str(8).empty() && !lineMatch.str(9).empty()){
-				arg2.op = SUM;
-				arg2.op2 = strToLower(lineMatch.str(9));
+				arg[1].op = SUM;
+				arg[1].op2 = strToLower(lineMatch.str(9));
 			}
-			else arg2.op = NONE;
-			statement.arg1 = arg1;
-			statement.arg2 = arg2;
-			statement.comment = lineMatch.str(10);
+			else arg[1].op = NONE;
+			arg[2].op1 = strToLower(lineMatch.str(10));
+			if(!lineMatch.str(11).empty() && !lineMatch.str(12).empty()){
+				arg[2].op = SUM;
+				arg[2].op2 = strToLower(lineMatch.str(12));
+			}
+			else arg[2].op = NONE;
+			statement.arg[0] = arg[0];
+			statement.arg[1] = arg[1];
+			statement.arg[2] = arg[2];
+			statement.comment = lineMatch.str(13);
 			statement.lineNumber = lineNumber;
 			
 			statements.push_back(statement);
@@ -93,7 +158,8 @@ int main(int argc, char** argv) {
 	list<Statement> statements;
 	list<Statement> ppStatements;
 	map<string, Symbol> ppts;
-	map<string, MDT> ppMDT;
+	map<string, MDT> ppMNT;
+	ppState state = NORMAL;
 	char ok;
 	
 	if(argc < 2) {
@@ -106,46 +172,98 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	statements = getStatementList(&file);
-//	for(Statement &it: statements){
-//		cout << "Line: \"" << it.line << "\"\n";
-//		cout << "\tLine Number: " << it.lineNumber << "\n";
-//		cout << "\tLabel: \"" << it.label << "\"\n";
-//		cout << "\tOperation: \"" << it.op << "\"\n";
-//		cout << "\tArg1_op1: \"" << it.arg1.op1 << "\"\n";
-//		cout << "\tArg1_op: " << (it.arg1.op ? "SUM" : "NONE") << "\n";
-//		cout << "\tArg1_op2: \"" << it.arg1.op2 << "\"\n";
-//		cout << "\tArg2_op1: \"" << it.arg2.op1 << "\"\n";
-//		cout << "\tArg2_op: " << (it.arg2.op ? "SUM" : "NONE") << "\n";
-//		cout << "\tArg2_op2: \"" << it.arg2.op2 << "\"\n";
-//		cout << "\tComment: \"" << it.comment << "\"\n";
-//	}
+	for(Statement &it: statements){
+		//it.print();
+	}
 	file.close();
 	
 	
+	MDT mdt;
+	list<Statement> macroList;
+	string macrolabel;
+	list<Statement> pptoRender;
+	
 	for(Statement &it: statements){
-		if(it.op == "equ"){
-			if(it.label.empty()){
-				cout << "Syntax Error: EQU requires a label on line " << it.lineNumber << "\n";
-				ok = 0;
-			}
-			if(it.arg1.op1.empty()){
-				cout << "Syntax Error: EQU requires one argument on line " << it.lineNumber << "\n";
-				ok = 0;
-			}
-			if(ok){
-				Symbol ns;
-				ns.value = atoi(it.arg1.op1.c_str());
-				ppts.insert(pair<string, Symbol>(it.label, ns));
-			}
+		switch(state){
+			case NORMAL:
+				ok = 1;
+				if(it.op == "equ"){
+					if(it.label.empty()){
+						cout << "Syntax Error: EQU requires a label on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					if(it.arg[0].op1.empty()){
+						cout << "Syntax Error: EQU requires one argument on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					if(ok){
+						Symbol ns;
+						ns.str = it.arg[0].op1;
+						ppts.insert(pair<string, Symbol>(it.label, ns));
+					}
+				}
+				else if(it.op == "macro"){
+					if(it.label.empty()){
+						cout << "Syntax Error: MACRO requires a label on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					if(it.arg[0].op != NONE || it.arg[1].op != NONE || it.arg[2].op != NONE){
+						cout << "Syntax Error: Sum expression can't be macro argument on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					if(ppMNT.count(it.label) > 0){
+						cout << "Semantic Error: Macro " << it.label << " already defined. Redefined on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					// !TODO: Detect number of arguments
+					if(ok){
+						mdt.nargs = it.countArgs();
+						mdt.arg[0] = it.arg[0].op1;
+						mdt.arg[1] = it.arg[1].op1;
+						mdt.arg[2] = it.arg[2].op1;
+						macrolabel = it.label;
+						state = MACRO;
+					}
+					
+				}
+				
+				else if(ppMNT.count(it.op) > 0){
+					mdt = ppMNT[it.op];
+					if(it.countArgs() != mdt.nargs){
+						cout << "Semantic Error: Macro " << it.op << " requires " << mdt.nargs << " arguments " << it.countArgs() << " given, on line " << it.lineNumber << "\n";
+						ok = 0;
+					}
+					if(ok){
+						map<string,Symbol> macroMap;
+						for(int i=0 ; i<it.countArgs() ; i++){
+							Symbol toinsert;
+							toinsert.str = it.arg[i].op1;
+							macroMap.insert(pair<string,Symbol>(mdt.arg[i], toinsert));
+						}
+						
+						for(Statement &it : mdt.lstmt){
+							Statement a = it.subst(macroMap).subst(ppts);
+							pptoRender.push_back(a);
+							a.print();
+						}
+					}
+				}
+				
+			break;
+			case MACRO:
+				if(it.op == "endmacro"){
+					mdt.lstmt = macroList;
+					ppMNT.insert(pair<string, MDT>(macrolabel, mdt));
+					state = NORMAL;
+					macroList.clear();
+				}
+				else macroList.push_back(it);
+			break;
+		
 		}
-		else if(it.op == "macro"){
-			if(it.label.empty()){
-				cout << "Syntax Error: MACRO requires a label on line " << it.lineNumber << "\n";
-				ok = 0;
-			}
-			
-		}
+
 	}
+
 	
 	
 	return 0;
